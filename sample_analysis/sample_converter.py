5c6e42604b9be086381d52e9d2bc006920176563
kingusiu <3071710+kingusiu@users.noreply.github.com>
2021-04-07 23:05:37 +0200

    updating particles to images conversion for single events; changing particle to jet image conversion to apply on per jet (not per event) basis

--- sample_analysis/sample_converter.py
+++ sample_analysis/sample_converter.py
@@ -1,7 +1,13 @@
 import numpy as np
 
 
-def bin_data_to_image(self, events, bin_borders, bins_n):
+def bin_data_to_image(events, bin_borders, bins_n=32):
+
+    # import ipdb; ipdb.set_trace()
+
+    # if just single event, expand to first axis
+    if len(events.shape) < 3:
+        events = events[np.newaxis,:,:]
 
     eta_idx, phi_idx, pt_idx = range(3)
     eventImagesShape = (events.shape[0], bins_n, bins_n)
@@ -14,14 +20,19 @@ def bin_data_to_image(self, events, bin_borders, bins_n):
         for particle in range(event.shape[0]):
             images[eventNo, binIdxEta[particle], binIdxPhi[particle]] += event[particle, pt_idx]  # add pt to bin of jet image
 
-    return images
+    return np.squeeze(images) # remove trailing dimensions if expanded previously
 
 
-def convert_jet_particles_to_jet_image(particles_j1, partciles_j2, bins_n=32):
-    ''' convert a dataset of jet1 and jet2 particle lists to jet images binned by dEta and dPhi '''
+def convert_jet_particles_to_jet_image(particles, bins_n=32):
+    ''' convert a dataset of jet particle lists to jet images binned by dEta and dPhi '''
 
     minAngle = -0.8;
     maxAngle = 0.8
     bin_borders = np.linspace(minAngle, maxAngle, num=bins_n)  # bins for eta & phi
 
-    return [bin_data_to_image(particles_j1, bin_borders, bins_n), bin_data_to_image(particles_j2, bin_borders, bins_n)]
+    return bin_data_to_image(particles, bin_borders, bins_n)
+
+
+def convert_event_sample_to_jet_image_j1j2(event_sample):
+    p1, p2 = event_sample.get_particles()
+    return convert_jet_particles_to_jet_image(p1), convert_jet_particles_to_jet_image(p2)
